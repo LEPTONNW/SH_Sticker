9801a11d3d3f054ca4e99792c8f096119b5eb75c
Round Robin <kac4484@gmail.com>
2021-05-20 21:22:35 +0900

    Add files via upload

---
+++ SH_Sticker/MainForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SH_Sticker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.NewM = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.새메모ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.옵션ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            
+            // 
+            // NewM
+            // 
+            this.NewM.BackColor = System.Drawing.Color.White;
+            this.NewM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NewM.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.NewM.Location = new System.Drawing.Point(12, 12);
+            this.NewM.Name = "NewM";
+            this.NewM.Size = new System.Drawing.Size(163, 121);
+            this.NewM.TabIndex = 2;
+            this.NewM.Text = "새 메모";
+            this.NewM.UseVisualStyleBackColor = false;
+            this.NewM.Click += new System.EventHandler(this.NewM_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(199, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "시작프로그램등록";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(199, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "시작프로그램등록해제";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("돋움", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button3.Location = new System.Drawing.Point(293, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 65);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "전체삭제";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(12, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 64);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "파일추출";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "해당 프로그램은 트레이상태로 됩니다.";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "SH_Sticker";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.새메모ToolStripMenuItem,
+            this.옵션ToolStripMenuItem,
+            this.종료ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(124, 76);
+            // 
+            // 새메모ToolStripMenuItem
+            // 
+            this.새메모ToolStripMenuItem.Name = "새메모ToolStripMenuItem";
+            this.새메모ToolStripMenuItem.Size = new System.Drawing.Size(123, 24);
+            this.새메모ToolStripMenuItem.Text = "새메모";
+            this.새메모ToolStripMenuItem.Click += new System.EventHandler(this.새메모ToolStripMenuItem_Click);
+            // 
+            // 옵션ToolStripMenuItem
+            // 
+            this.옵션ToolStripMenuItem.Name = "옵션ToolStripMenuItem";
+            this.옵션ToolStripMenuItem.Size = new System.Drawing.Size(123, 24);
+            this.옵션ToolStripMenuItem.Text = "옵션";
+            this.옵션ToolStripMenuItem.Click += new System.EventHandler(this.옵션ToolStripMenuItem_Click);
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(123, 24);
+            this.종료ToolStripMenuItem.Text = "종료";
+            this.종료ToolStripMenuItem.Click += new System.EventHandler(this.종료ToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(394, 253);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.NewM);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "SH_Sticker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button NewM;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 새메모ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 옵션ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
+    }
+}
+
---
+++ SH_Sticker/MainForm.cs
@@ -0,0 +1,306 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SH_Sticker
+{
+    public partial class MainForm : Form
+    {
+        public string PB_Path = "C:\\Program Files\\SH Sticker";
+        public int Ex = 0;
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.ShowInTaskbar = false;
+            this.Visible = false;
+            this.notifyIcon1.Visible = true;
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string procName = Process.GetCurrentProcess().ProcessName;
+
+            Process[] procArray = Process.GetProcessesByName(procName);
+            if (procArray.Length > 1)
+            {
+                MessageBox.Show("Error : 관리자 권한으로 실행되지 않았거나, 이미 실행중입니다.");
+                Exit();
+            }
+            else
+            {
+
+            }
+
+            if (File.Exists("C:\\Program Files\\SH Sticker\\data\\.ini"))
+            {
+                File.Delete("C:\\Program Files\\SH Sticker\\data\\.ini");
+
+                if(File.Exists("C:\\Program Files\\SH Sticker\\data\\.txt"))
+                {
+                    File.Delete("C:\\Program Files\\SH Sticker\\data\\.txt");
+                }
+
+            }
+
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowIcon = false; //작업표시줄에서 제거.
+            notifyIcon1.Visible = true; //트레이 아이콘을 표시한다.
+            
+            if (!Directory.Exists(PB_Path))
+            {
+                Directory.CreateDirectory(PB_Path);
+                Directory.CreateDirectory(PB_Path + "\\data");
+                Directory.CreateDirectory(PB_Path + "\\recycle");
+            }
+            else
+            {
+                try
+                {
+                    DirectoryInfo DIR = new DirectoryInfo(PB_Path + "\\data\\");
+                    FileInfo[] F = DIR.GetFiles("*.ini", SearchOption.AllDirectories);
+                    FileInfo[] FT = DIR.GetFiles("*.txt", SearchOption.AllDirectories);
+                    int co = 0;
+                    foreach (FileInfo file in F)
+                    {
+                        string name = file.Name.ToString();
+                        string[] Fname = name.Split('.'); //파일이름
+                        TextMemo frm2 = new TextMemo();
+
+                        frm2.FNA = Fname[0];
+                        frm2.FNA2 = "1";
+
+                        string TP1 = Decrypt("K", File.ReadAllText(PB_Path + "\\data\\" + FT[co]));//내용
+                        frm2.FNA3 = TP1;
+
+                        StringBuilder Ptext2 = new StringBuilder();
+                        int ret2 = GetPrivateProfileString("Section2", "BgColor", "", Ptext2, 2147483647, PB_Path + "\\data\\" + file); //색깔코드
+                        frm2.FNA4 = Ptext2.ToString();
+
+                        StringBuilder Ptext3 = new StringBuilder();
+                        int ret3 = GetPrivateProfileString("Section3", "title", "", Ptext3, 2147483647, PB_Path + "\\data\\" + file); //제목
+                        string TP2 = Decrypt("K", Ptext3.ToString());
+                        frm2.FNA5 = TP2;
+
+                        StringBuilder Ptext4 = new StringBuilder();
+                        int ret4 = GetPrivateProfileString("Section4", "Location", "", Ptext4, 2147483647, PB_Path + "\\data\\" + file); //위치정보
+                        string TP3 = Decrypt("K", Ptext4.ToString());
+                        frm2.FAN6 = TP3;
+
+                        StringBuilder Ptext5 = new StringBuilder();
+                        int ret5 = GetPrivateProfileString("Section5", "Size", "", Ptext5, 2147483647, PB_Path + "\\data\\" + file); //사이즈
+                        frm2.FAN7 = Ptext5.ToString();
+
+                        frm2.Show();
+                        co++;
+                    }
+                    co = 0;
+                }
+                catch
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+        }
+        //한 때 경로지정 기능이였음
+        //folderBrowserDialog1.ShowDialog();
+        //string Path = folderBrowserDialog1.SelectedPath;
+        //PAT_Box1.Text = Path;
+        private void NewM_Click(object sender, EventArgs e)
+        {
+            var myForm = new TextMemo();
+            myForm.Show();
+        }
+        public static string Encrypt(string key, string data) //복호화
+        {
+            Encoding unicode = Encoding.Unicode;
+            return Convert.ToBase64String(Encrypt(unicode.GetBytes(key), unicode.GetBytes(data)));
+        }
+        public static string Decrypt(string key, string data)
+        {
+            Encoding unicode = Encoding.Unicode;
+            return unicode.GetString(Encrypt(unicode.GetBytes(key), Convert.FromBase64String(data)));
+        }
+        public static byte[] Encrypt(byte[] key, byte[] data)
+        {
+            return EncryptOutput(key, data).ToArray();
+        }
+        public static byte[] Decrypt(byte[] key, byte[] data)
+        {
+            return EncryptOutput(key, data).ToArray();
+        }
+        private static byte[] EncryptInitalize(byte[] key)
+        {
+            byte[] s = Enumerable.Range(0, 256)
+              .Select(i => (byte)i)
+              .ToArray();
+
+            for (int i = 0, j = 0; i < 256; i++)
+            {
+                j = (j + key[i % key.Length] + s[i]) & 255;
+                Swap(s, i, j);
+            }
+            return s;
+        }
+        private static IEnumerable<byte> EncryptOutput(byte[] key, IEnumerable<byte> data)
+        {
+            byte[] s = EncryptInitalize(key);
+            int i = 0;
+            int j = 0;
+
+            return data.Select((b) =>
+            {
+                i = (i + 1) & 255;
+                j = (j + s[i]) & 255;
+                Swap(s, i, j);
+                return (byte)(b ^ s[(s[i] + s[j]) & 255]);
+            });
+        }
+
+        private static void Swap(byte[] s, int i, int j)
+        {
+            byte c = s[i];
+            s[i] = s[j];
+            s[j] = c;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey rkey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            rkey.SetValue("ProgramName", Application.ExecutablePath.ToString());
+            MessageBox.Show("시작프로그램으로 등록되었습니다.");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey rkey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            rkey.DeleteValue("ProgramName", false);
+            MessageBox.Show("시작프로그램 등록해제되었습니다.");
+        }
+        private void button3_Click(object sender, EventArgs e) //전체삭제버튼
+        {
+            if (MessageBox.Show("정말로 전체삭제 하시겠습니까?"+Environment.NewLine+"확인을 누르면 모든 메모가 삭제됩니다.", "확인 취소", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                if(MessageBox.Show("정말로 삭제를 하시는게 맞나요?", "확인 취소", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        DirectoryInfo DR = new DirectoryInfo(PB_Path + "\\data");
+                        FileInfo[] GetF = DR.GetFiles("*.*", SearchOption.AllDirectories);
+                        foreach (FileInfo FI in GetF)
+                        {
+                            File.Delete(PB_Path + "\\data\\" + FI);
+                        }
+                    }
+                    catch
+                    {
+                        Exit();
+                    }
+                    Exit();
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+        void Exit()
+        {
+            Ex = 1;
+            Application.ExitThread();
+            Application.Exit();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("C:\\Program Files\\SH Sticker\\data\\.ini"))
+            {
+                File.Delete("C:\\Program Files\\SH Sticker\\data\\.ini");
+
+                if (File.Exists("C:\\Program Files\\SH Sticker\\data\\.txt"))
+                {
+                    File.Delete("C:\\Program Files\\SH Sticker\\data\\.txt");
+                }
+
+            }
+
+            string RPath = null;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                RPath = folderBrowserDialog1.SelectedPath;
+                DirectoryInfo R = new DirectoryInfo(PB_Path + "\\data\\");
+                FileInfo[] FR = R.GetFiles("*.ini", SearchOption.AllDirectories);
+                FileInfo[] FRT = R.GetFiles("*.txt", SearchOption.AllDirectories);
+
+                int Dco = 0;
+                foreach (FileInfo Rfile in FR)
+                {
+                    StringBuilder INIInfo = new StringBuilder();
+                    int ret0 = GetPrivateProfileString("Section3", "title", "", INIInfo, 2147483647, PB_Path + "\\data\\" + Rfile); //제목
+                    string DTitle = Decrypt("K", INIInfo.ToString()) + ".txt";
+                    string DText = Decrypt("K", File.ReadAllText(PB_Path + "\\data\\" + FRT[Dco]));//내용
+                    File.AppendAllText(RPath + "\\" + DTitle, DText);
+                    Dco++;
+                }
+                Dco = 0;
+            }
+            else
+            {
+                RPath = null;
+                return;
+            }
+        }
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true; // 폼의 표시
+            if (this.WindowState == FormWindowState.Minimized)
+                this.WindowState = FormWindowState.Normal; // 최소화를 멈춘다 
+            this.Activate(); // 폼을 활성화 시킨다
+        }
+
+        private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Exit();
+        }
+
+        private void 옵션ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Visible = true; // 폼의 표시
+            if (this.WindowState == FormWindowState.Minimized)
+                this.WindowState = FormWindowState.Normal; // 최소화를 멈춘다 
+            this.Activate(); // 폼을 활성화 시킨다
+        }
+
+        private void 새메모ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var myForm = new TextMemo();
+            myForm.Show();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+
+            this.Hide();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowIcon = false; //작업표시줄에서 제거.
+            notifyIcon1.Visible = true; //트레이 아이콘을 표시한다.
+            if (Ex == 0) { notifyIcon1.ShowBalloonTip(1000); }
+        }
+    }
+}
