5f70d526be56d28549aae31008f61d52fde7d716
Round Robin <kac4484@gmail.com>
2021-05-20 23:06:29 +0900

    Add files via upload

---
+++ SH_Sticker/TextMemo.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace SH_Sticker
+{
+    partial class TextMemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextMemo));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ExButton = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SCColor = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(8, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "제목 :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ExButton
+            // 
+            this.ExButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExButton.BackColor = System.Drawing.Color.Red;
+            this.ExButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ExButton.Font = new System.Drawing.Font("돋움", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ExButton.ForeColor = System.Drawing.Color.White;
+            this.ExButton.Location = new System.Drawing.Point(419, -5);
+            this.ExButton.Name = "ExButton";
+            this.ExButton.Size = new System.Drawing.Size(36, 35);
+            this.ExButton.TabIndex = 3;
+            this.ExButton.Text = "X";
+            this.ExButton.UseVisualStyleBackColor = false;
+            this.ExButton.Click += new System.EventHandler(this.ExButton_Click);
+            // 
+            // Save
+            // 
+            this.Save.Font = new System.Drawing.Font("굴림", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Save.Location = new System.Drawing.Point(385, 36);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(54, 27);
+            this.Save.TabIndex = 5;
+            this.Save.Text = "저장";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // SCColor
+            // 
+            this.SCColor.BackColor = System.Drawing.Color.White;
+            this.SCColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SCColor.Location = new System.Drawing.Point(0, 0);
+            this.SCColor.Name = "SCColor";
+            this.SCColor.Size = new System.Drawing.Size(39, 26);
+            this.SCColor.TabIndex = 6;
+            this.SCColor.Text = "색";
+            this.SCColor.UseVisualStyleBackColor = false;
+            this.SCColor.Click += new System.EventHandler(this.SCColor_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(11, 78);
+            this.richTextBox1.Multiline = true;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(426, 376);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("굴림", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(45, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Text";
+            this.label2.DoubleClick += new System.EventHandler(this.label2_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("돋움", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(384, -2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 32);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "ㅡ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TextMemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(450, 466);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.SCColor);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ExButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "TextMemo";
+            this.Load += new System.EventHandler(this.TextMemo_Load);
+            this.Move += new System.EventHandler(this.TextMemo_Move);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ExButton;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button SCColor;
+        private System.Windows.Forms.TextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ SH_Sticker/TextMemo.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SH_Sticker
+{
+    public partial class TextMemo : Form
+    {
+        public TextMemo()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.ShowInTaskbar = false;
+
+        }
+        [DllImport("kernel32")] private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")] private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        private string TextMemo_value;
+        public string MoFname, MoText, MoColor, Motitle, MoLocation, MoSize;
+
+        public string FilePath = "C:\\Program Files\\SH Sticker\\data", filename, ColCode = "";
+        public int Model, temp, x, Count = 0, StCount = 0;
+        private const int cGrip = 16;      // 그립사이즈
+        private const int cCaption = 32;   // 캡션 바 높이
+        public string FNA, FNA2 = "0", FNA3, FNA4, FNA5, FAN6, FAN7; //전달값부분
+
+        protected override void WndProc(ref Message m) //FormboaderStyle 이 None 일 경우 마우스제어, 크기조절 함수
+        {
+            if (m.Msg == 0x84)
+            {  // Trap WM_NCHITTEST
+                Point pos = new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16); //크기조절
+                pos = this.PointToClient(pos); //Y축
+                if (pos.Y < cCaption)
+                {
+                    m.Result = (IntPtr)2;  // HTCAPTION
+                    return;
+                }
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip) //X축
+                {
+                    m.Result = (IntPtr)17; // HTBOTTOMRIGHT
+                    return;
+                }
+                switch (m.Msg) //마우스 제어
+                {
+                    case 0x84:
+                        base.WndProc(ref m);
+                        if ((int)m.Result == 0x1)
+                            m.Result = (IntPtr)0x2;
+                        return;
+                }
+            }
+            base.WndProc(ref m); //반환
+        }
+        protected override void OnPaint(PaintEventArgs e) //크기조절 컨트롤 모양
+        {
+            Rectangle rc = new Rectangle(this.ClientSize.Width - cGrip, this.ClientSize.Height - cGrip, cGrip, cGrip);
+            ControlPaint.DrawSizeGrip(e.Graphics, this.BackColor, rc);
+            rc = new Rectangle(0, 0, this.ClientSize.Width, cCaption);
+            //e.Graphics.FillRectangle(Brushes.DarkBlue, rc);
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData) //단축키 제어
+        {
+            if (!base.ProcessCmdKey(ref msg, keyData))
+            {
+                if (keyData.Equals(Keys.Control | Keys.A)) {richTextBox1.SelectAll(); return true;}
+                else if (keyData.Equals(Keys.Control | Keys.F4)) { return true; }
+                else { return false; }
+            }
+            else { return true; }
+        }
+        public void Start() //메모Form시작시 실행되는 코드
+        {
+            this.MinimumSize = new Size(350, 30);
+            Model = 0; //새 폼과 기존데이터를 불러온 폼을 구별하기위한 변수
+
+            MoFname = FNA; //파일이름받는구간
+            Model = Convert.ToInt32(FNA2); //Model값 받는 구간
+            MoText = FNA3; if (Model == 1) { richTextBox1.Text = MoText; } //텍스트 받는 구간
+
+            MoColor = FNA4;//컬러코드 받는 구간
+            try
+            {
+                if (Model == 1) //폼의 색을 결정하기위해 MainForm에서 받은 RGB데이터를 가공하는 함수
+                {
+                    var p = MoColor.Split(new char[] { ',', ']' }); 
+                    int A = Convert.ToInt32(p[0].Substring(p[0].IndexOf('=') + 1));
+                    int R = Convert.ToInt32(p[1].Substring(p[1].IndexOf('=') + 1));
+                    int G = Convert.ToInt32(p[2].Substring(p[2].IndexOf('=') + 1));
+                    int B = Convert.ToInt32(p[3].Substring(p[3].IndexOf('=') + 1));
+                    this.BackColor = Color.FromArgb(A, R, G, B);
+                    ColCode = this.BackColor.ToString();
+                }
+                else { ColCode = this.BackColor.ToString(); }
+            }
+            catch
+            {
+                try
+                {
+                    var p = MoColor.Split(new char[] { ',', ']' }); //위와 동일하기 Color코드에 대한 데이터를 받지만 지정된 "아쿠아색"같은 색을 받는 예외처리이다.
+                    string c = Convert.ToString(p[0].Substring(p[0].IndexOf('[') + 1));
+                    this.BackColor = Color.FromName(c);
+                }
+                catch
+                {
+                }
+            }
+            
+            Motitle = FNA5; //제목값받는구간
+            if (Model == 1) { label2.Text = Motitle; textBox1.Text = Motitle; }
+            MoLocation = FAN6; //폼위치정보받는구간
+            if (Model == 1) //MainForm 에서 받은 폼의 X,Y축 위치데이터를 가공하는 함수이다.
+            {
+                var q = MoLocation.Split(new char[] { ',', '}' });
+                int X = Convert.ToInt32(q[0].Substring(q[0].IndexOf('=') + 1));
+                int Y = Convert.ToInt32(q[1].Substring(q[1].IndexOf('=') + 1));
+                this.Location = new Point(X, Y);
+            }
+            MoSize = FAN7; //폼사이즈받는구간
+            if (MoSize == "1") { this.Size = MinimumSize; }
+            else { this.Size = new Size(400, 443);}
+            Save.Hide();
+            StCount = 1;
+
+            filename = "\\" + MoFname + ".ini";
+            string[] ttpath = filename.Split('.');
+            string textname = ttpath[0] + ".txt";
+        }
+        /// <summary>
+        /// 파일 저장구간
+        /// </summary>
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (StCount == 0) { Start();}
+
+            string STR = Encrypt("K", richTextBox1.Text);
+            string TL = Encrypt("K", textBox1.Text);
+            string LO = Encrypt("K", this.Location.ToString());
+
+            if (Model == 0)
+            {
+                filename = "\\" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".ini";
+                string[] ttpath = filename.Split('.');
+                string textname = ttpath[0] + ".txt";
+                File.AppendAllText(FilePath + textname, STR);
+                WritePrivateProfileString("Section2", "BgColor", ColCode, FilePath + filename);
+                WritePrivateProfileString("Section3", "title", TL, FilePath + filename);
+                WritePrivateProfileString("Section4", "Location", LO, FilePath + filename);
+                Model = 2;
+            }
+            else if (Model == 1)
+            {
+                filename = "\\" + MoFname + ".ini";
+                string[] ttpath = filename.Split('.');
+                string textname = ttpath[0] + ".txt";
+                File.Delete(FilePath + textname);
+                File.AppendAllText(FilePath + textname, STR);
+                WritePrivateProfileString("Section2", "BgColor", ColCode, FilePath + filename);
+                WritePrivateProfileString("Section3", "title", TL, FilePath + filename);
+                WritePrivateProfileString("Section4", "Location", LO, FilePath + filename);
+                Model = 2;
+            }
+            else if (Model == 2)
+            {
+                string[] ttpath = filename.Split('.');
+                string textname = ttpath[0] + ".txt";
+                File.Delete(FilePath + textname);
+                File.AppendAllText(FilePath + textname, STR);
+                WritePrivateProfileString("Section2", "BgColor", ColCode, FilePath + filename);
+                WritePrivateProfileString("Section3", "title", TL, FilePath + filename);
+                WritePrivateProfileString("Section4", "Location", LO, FilePath + filename);
+            }
+            else { MessageBox.Show("Error");}
+            Thread.Sleep(5);
+        }
+        private void TextMemo_Move(object sender, EventArgs e)
+        {
+            string LO = Encrypt("K", this.Location.ToString());
+            WritePrivateProfileString("Section4", "Location", LO, FilePath + filename);
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            string STR = Encrypt("K", richTextBox1.Text);
+            string TL = Encrypt("K", textBox1.Text);
+            string LO = Encrypt("K", this.Location.ToString());
+
+            if (Model == 0)
+            {
+                filename = "\\" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".ini";
+                string[] ttpath = filename.Split('.');
+                string textname = ttpath[0] + ".txt";
+                File.AppendAllText(FilePath + textname, STR);
+                WritePrivateProfileString("Section2", "BgColor", ColCode, FilePath + filename);
+                WritePrivateProfileString("Section3", "title", TL, FilePath + filename);
+                WritePrivateProfileString("Section4", "Location", LO, FilePath + filename);
+                Model = 2;
+            }
+            else if (Model == 1)
+            {
+                filename = "\\" + MoFname + ".ini";
+                string[] ttpath = filename.Split('.');
+                string textname = ttpath[0] + ".txt";
+                File.Delete(FilePath + textname);
+                File.AppendAllText(FilePath + textname, STR);
+                WritePrivateProfileString("Section2", "BgColor", ColCode, FilePath + filename);
+                WritePrivateProfileString("Section3", "title", TL, FilePath + filename);
+                WritePrivateProfileString("Section4", "Location", LO, FilePath + filename);
+                Model = 2;
+            }
+            else if (Model == 2)
+            {
+                string[] ttpath = filename.Split('.');
+                string textname = ttpath[0] + ".txt";
+                File.Delete(FilePath + textname);
+                File.AppendAllText(FilePath + textname, STR);
+                WritePrivateProfileString("Section2", "BgColor", ColCode, FilePath + filename);
+                WritePrivateProfileString("Section3", "title", TL, FilePath + filename);
+                WritePrivateProfileString("Section4", "Location", LO, FilePath + filename);
+            }
+            else { MessageBox.Show("Error"); }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (StCount == 0) { Start(); }
+
+            label2.Text = textBox1.Text;
+            string STR = Encrypt("K", richTextBox1.Text);
+            string TL = Encrypt("K", textBox1.Text);
+            string LO = Encrypt("K", this.Location.ToString());
+
+            if (Model == 0)
+            {
+                filename = "\\" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".ini";
+                string[] ttpath = filename.Split('.');
+                string textname = ttpath[0] + ".txt";
+                File.AppendAllText(FilePath + textname, STR);
+                WritePrivateProfileString("Section2", "BgColor", ColCode, FilePath + filename);
+                WritePrivateProfileString("Section3", "title", TL, FilePath + filename);
+                WritePrivateProfileString("Section4", "Location", LO, FilePath + filename);
+                Model = 2;
+            }
+            else if (Model == 1)
+            {
+                filename = "\\" + MoFname + ".ini";
+                string[] ttpath = filename.Split('.');
+                string textname = ttpath[0] + ".txt";
+                File.Delete(FilePath + textname);
+                File.AppendAllText(FilePath + textname, STR);
+                WritePrivateProfileString("Section2", "BgColor", ColCode, FilePath + filename);
+                WritePrivateProfileString("Section3", "title", TL, FilePath + filename);
+                WritePrivateProfileString("Section4", "Location", LO, FilePath + filename);
+                Model = 2;
+            }
+            else if (Model == 2)
+            {
+                string[] ttpath = filename.Split('.');
+                string textname = ttpath[0] + ".txt";
+                File.Delete(FilePath + textname);
+                File.AppendAllText(FilePath + textname, STR);
+                WritePrivateProfileString("Section2", "BgColor", ColCode, FilePath + filename);
+                WritePrivateProfileString("Section3", "title", TL, FilePath + filename);
+                WritePrivateProfileString("Section4", "Location", LO, FilePath + filename);
+            }
+            else { MessageBox.Show("Error"); }
+            Thread.Sleep(5);
+        }
+
+        /// <summary>
+        /// 버튼별 이벤트 구간
+        /// </summary>
+        private void TextMemo_Load(object sender, EventArgs e) { Start();}
+        private void SCColor_Click(object sender, EventArgs e)
+        {
+            this.BackColor = MirSelColor();
+            ColCode = this.BackColor.ToString();
+            WritePrivateProfileString("Section2", "BgColor", ColCode, FilePath + filename);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.Size == MinimumSize)
+            {
+                this.Size = new Size(400, 443);
+                Count = 0;
+                WritePrivateProfileString("Section5", "Size", Count.ToString(), FilePath + filename);
+            }
+            else
+            {
+                this.Size = MinimumSize;
+                Count = 1;
+                WritePrivateProfileString("Section5", "Size", Count.ToString(), FilePath + filename);
+            }
+        }
+        private void ExButton_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("삭제하시겠습니까?", "확인 취소", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                //DirectoryInfo BDIR = new DirectoryInfo(FilePath + "\\");
+                //FileInfo[] F = BDIR.GetFiles("*.ini", SearchOption.AllDirectories);
+                try { File.Delete("C:\\Program Files\\SH Sticker\\data\\.ini"); }
+                catch { }
+                
+
+                try
+                {
+                    string[] ttpath = filename.Split('.');
+                    string textname = ttpath[0] + ".txt";
+                    File.Delete(FilePath + filename);
+                    File.Delete(FilePath + textname);
+                }
+                catch { Exit(); }
+            }
+            else { return; }
+            Exit();
+        }
+
+        /// <summary>
+        /// 컨트롤 이벤트 구간
+        /// </summary>
+        private void textBox1_Leave(object sender, EventArgs e) { label2.Text = textBox1.Text; }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.Size == MinimumSize)
+            {
+                this.Size = new Size(400, 443);
+                Count = 0;
+                WritePrivateProfileString("Section5", "Size", Count.ToString(), FilePath + filename);
+            }
+            else
+            {
+                this.Size = MinimumSize;
+                Count = 1;
+                WritePrivateProfileString("Section5", "Size", Count.ToString(), FilePath + filename);
+            }
+        }
+
+        /// <summary>
+        /// 암호화 구간
+        /// </summary>
+        public static string Encrypt(string key, string data) //RC4 암호화
+        {
+            Encoding unicode = Encoding.Unicode;
+            return Convert.ToBase64String(Encrypt(unicode.GetBytes(key), unicode.GetBytes(data)));
+        }
+        public static string Decrypt(string key, string data)
+        {
+            Encoding unicode = Encoding.Unicode;
+            return unicode.GetString(Encrypt(unicode.GetBytes(key), Convert.FromBase64String(data)));
+        }
+        public static byte[] Encrypt(byte[] key, byte[] data) { return EncryptOutput(key, data).ToArray();}
+        public static byte[] Decrypt(byte[] key, byte[] data) { return EncryptOutput(key, data).ToArray();}
+        private static byte[] EncryptInitalize(byte[] key)
+        {
+            byte[] s = Enumerable.Range(0, 256)
+              .Select(i => (byte)i)
+              .ToArray();
+
+            for (int i = 0, j = 0; i < 256; i++)
+            {
+                j = (j + key[i % key.Length] + s[i]) & 255;
+                Swap(s, i, j);
+            }
+            return s;
+        }
+
+        private static IEnumerable<byte> EncryptOutput(byte[] key, IEnumerable<byte> data)
+        {
+            byte[] s = EncryptInitalize(key);
+
+            int i = 0;
+            int j = 0;
+
+            return data.Select((b) =>
+            {
+                i = (i + 1) & 255;
+                j = (j + s[i]) & 255;
+                Swap(s, i, j);
+                return (byte)(b ^ s[(s[i] + s[j]) & 255]);
+            });
+        }
+
+        private static void Swap(byte[] s, int i, int j)
+        {
+            byte c = s[i];
+            s[i] = s[j];
+            s[j] = c;
+        }
+
+        /// <summary>
+        /// 일반 함수 구간
+        /// </summary>
+        /// <returns>색상을 선택하지 않았을경우 Black리턴</returns>
+        public Color MirSelColor() ///색상 선택창을 로드후 선택한 Color를 리턴한다.
+        {
+
+            if (colorDialog1.ShowDialog() == DialogResult.OK) { return colorDialog1.Color;}
+            else { return this.BackColor; }
+        }
+
+        public void Exit() { this.Close(); }
+    }
+}
